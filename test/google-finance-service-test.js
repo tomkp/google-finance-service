34ca942ed0c5662233b3173445691a80914b3a37
tomkp <tom@tomkp.com>
2013-11-30 18:28:19 +0000

    use callbacks

--- test/google-finance-service-test.js
+++ test/google-finance-service-test.js
@@ -5,36 +5,30 @@ var service = require('../lib/google-finance-service.js');
 describe('Google Finance Service', function () {
 
     it('fetches for comma separated list of symbols', function (done) {
-        $.when(
-            service('LON:TSCO,NASDAQ:GOOG')
-        ).then(
-            function (data) {
-                console.info('success', data);
-                assert.equal(2, data.length);
-                assert.equal('Tesco PLC', data[0].name);
-                assert.equal('TSCO', data[0].t);
-                assert.equal('Google Inc', data[1].name);
-                assert.equal('GOOG', data[1].t);
-                done();
-            }
-        );
+
+        service('LON:TSCO,NASDAQ:GOOG', function (data) {
+            console.info('success', data);
+            assert.equal(2, data.length);
+            assert.equal('Tesco PLC', data[0].name);
+            assert.equal('TSCO', data[0].t);
+            assert.equal('Google Inc', data[1].name);
+            assert.equal('GOOG', data[1].t);
+            done();
+        });
     });
 
 
     it('fetches for array of symbols', function (done) {
-        $.when(
-            service(['LON:TSCO', 'NASDAQ:GOOG'])
-        ).then(
-            function (data) {
-                console.info('success', data);
-                assert.equal(2, data.length);
-                assert.equal('Tesco PLC', data[0].name);
-                assert.equal('TSCO', data[0].t);
-                assert.equal('Google Inc', data[1].name);
-                assert.equal('GOOG', data[1].t);
-                done();
-            }
-        );
+
+        service(['LON:TSCO', 'NASDAQ:GOOG'], function (data) {
+            console.info('success', data);
+            assert.equal(2, data.length);
+            assert.equal('Tesco PLC', data[0].name);
+            assert.equal('TSCO', data[0].t);
+            assert.equal('Google Inc', data[1].name);
+            assert.equal('GOOG', data[1].t);
+            done();
+        });
     });
 
 
